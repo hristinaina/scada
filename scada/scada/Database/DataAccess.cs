d7f231fc1ca14304f67ee57aec456457b4ef329b
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-08-02 01:08:36 +0200

    Database integration

---
+++ scada/scada/Database/DataAccess.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace scada.Database
+{
+    public class DataAccess
+    {
+        private readonly MySqlConnection _connection;
+
+        public DataAccess(MySqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void GetDataFromDatabase()
+        {
+            try
+            {
+                _connection.Open();
+                string query = "SELECT * FROM testable";
+                MySqlCommand command = new MySqlCommand(query, _connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // Process the data
+                    Console.WriteLine(reader.GetString(0));
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine("error!!!");
+            }
+        }
+    }
+}
