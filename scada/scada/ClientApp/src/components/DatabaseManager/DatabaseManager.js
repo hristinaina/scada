3a36608eb86286aa147f71bc231d2445eca7a171
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-08-24 13:57:43 +0200

    Implemented dropdown list on click

--- scada/scada/ClientApp/src/components/DatabaseManager/DatabaseManager.js
+++ scada/scada/ClientApp/src/components/DatabaseManager/DatabaseManager.js
@@ -8,15 +8,36 @@ export class DatabaseManager extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            isDropdownOpen: false, // Initialize dropdown state as closed
+        };
     }
 
+    toggleDropdown = () => {
+        this.setState((prevState) => ({
+            isDropdownOpen: !prevState.isDropdownOpen,
+        }));
+    };
+
     render() {
+        const { isDropdownOpen } = this.state;
         return (
             <div>
                 <NavMenu showNavbar={true} />
                 <h1 id="tableLabel">Database Manager</h1>
                 {/*<img alt="." src="../..images/plus.png"/>*/}
-                <p id="add-tag">Add tag</p>
+                <p id="add-tag" onClick={this.toggleDropdown}>Add tag</p>
+
+                {isDropdownOpen && (
+                    <div id="dropdown">
+                        <ul id="dropdown-tags">
+                            <li className="tag bottom-margin">ANALOG OUTPUT TAG</li>
+                            <li className="tag bottom-margin">ANALOG INPUT TAG</li>
+                            <li className="tag bottom-margin">DIGITAL OUTPUT TAG</li>
+                            <li className="tag">DIGITAL INPUT TAG</li>
+                        </ul>
+                    </div>
+                )}
             </div>
         );
     }
