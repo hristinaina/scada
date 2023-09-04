c9962c1274e744058c7e840c56cc8594b22e3625
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-09-04 13:22:32 +0200

    Binding front and back for getting all alarms
    Getting all alarms for specified tag

--- scada/scada/ClientApp/src/components/dialogs/Alarm/Alarms.js
+++ scada/scada/ClientApp/src/components/dialogs/Alarm/Alarms.js
@@ -1,16 +1,23 @@
-﻿import CreateAlarmDialog from '../CreateAlarm/CreateAlarmDialog';
-import './Alarms.css';
+﻿import './Alarms.css';
 import React, { useEffect, useState } from 'react';
-//import TagService from "../../services/TagService";
+import axios from 'axios';
 
 
 const Alarms = ({ onClose, openCreateAlarmDialog, tagId }) => {
     const [alarms, setAlarms] = useState([]);
 
-/*    useEffect( async() => {
-        // Fetch data here and update AOData
-        await TagService.getAlarms().then((data) => setAlarms());
-    }, []);*/
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get('http://localhost:5083/api/tag/alarm/' + tagId);
+                setAlarms(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        fetchData();
+    }, [alarms]);
 
     return (
         <div className="alarms-dialog">
